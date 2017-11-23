58a44df7d45a96431704dd92d9802103a5b1f6fb
Aygistov Ilya <I.Aygistov@sls.ua>
2017-11-23 16:50:30 +0200

    Ok

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var promise = mongoose.connect('mongodb://aygistov:Ayg020210@ds149535.mlab.com:49535/aygistovapp', {
+    useMongoClient: true,
+    /* other options */
+});
+var schema = new mongoose.Schema({ name: 'string', size: 'string' });
+var Items = mongoose.model('items', schema);
+var myItems = "123";
+var err;
+
+Items.findOne({ 'name': 'qwerty' }, 'name size', function (err, myItem) {
+    myItems = myItem.size;
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: myItems });
 });
 
 module.exports = router;
+
